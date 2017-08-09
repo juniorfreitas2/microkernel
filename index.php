276e07c708b633ee1cd98e6e95a3a81e9e6c2a67
Junior <web.juniorfreitas@gmail.com>
2017-08-09 13:35:32 -0300

    hello world into microkernel

--- index.php
+++ index.php
@@ -1,3 +1,44 @@
 <?php
 
 require __DIR__."/vendor/autoload.php";
+
+use \Symfony\Component\HttpKernel\Kernel;
+use \Symfony\Component\Config\Loader\LoaderInterface;
+use \Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+
+class appKernel extends Kernel{
+
+    use MicroKernelTrait;
+
+    public function registerBundles()
+    {
+        return [
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle()
+        ];
+    }
+
+    protected function configureContainer(\Symfony\Component\DependencyInjection\ContainerBuilder $c, LoaderInterface $loader)
+    {
+        $c->loadFromExtension('framework', [
+            'secret' => 'Secret_KEY',
+        ]);
+    }
+
+    protected function configureRoutes(\Symfony\Component\Routing\RouteCollectionBuilder $routes)
+    {
+        $routes->add('/', 'Kernel:index');
+    }
+
+    public function index()
+    {
+        return new \Symfony\Component\HttpFoundation\Response('Hello Microkernel');
+    }
+}
+
+    $kernel = new appKernel('dev', true);
+    $request = \Symfony\Component\HttpFoundation\Request::createFromGlobals();
+    $response = $kernel->handle($request);
+
+    $response->send();
+
+    $kernel->terminate($request, $response);
